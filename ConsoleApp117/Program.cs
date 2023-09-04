18f1b64a6f65e11de47088ef443b793e08ed1eff
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-04 10:18:35 +0330

    Add files via upload

---
+++ ConsoleApp117/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp117
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conditional AND (&&)");
+            Console.WriteLine($"false && false:{false && false}");
+            Console.WriteLine($"false && true:{false && true}");
+            Console.WriteLine($"true &&false:{true && false}");
+            Console.WriteLine($"true&&true:{true && true}\n");
+
+
+            Console.WriteLine("Conditional OR (||)");
+            Console.WriteLine($"false || false:{false || false}");
+            Console.WriteLine($"false || true:{false || true}");
+            Console.WriteLine($"true ||false:{true || false}");
+            Console.WriteLine($"true||true:{true || true}\n");
+
+
+            Console.WriteLine("Boolean logical AND (&)");
+            Console.WriteLine($"false & false:{false & false}");
+            Console.WriteLine($"false & true:{false & true}");
+            Console.WriteLine($"true & false:{true & false}");
+            Console.WriteLine($"true & true:{true & true}\n");
+
+            Console.WriteLine("Boolean logical inclusive OR(|)");
+            Console.WriteLine($"false | false:{false | false}");
+            Console.WriteLine($"false | true:{false | true}");
+            Console.WriteLine($"true | false:{true | false}");
+            Console.WriteLine($"true | true:{true | true}\n");
+
+
+            Console.WriteLine("Boolean logical exclusive OR(^)");
+            Console.WriteLine($"false ^ false:{false ^ false}");
+            Console.WriteLine($"false ^ true:{false ^ true}");
+            Console.WriteLine($"true ^ false:{true ^ false}");
+            Console.WriteLine($"true ^ true:{true ^ true}\n");
+
+            Console.WriteLine("Logical negation (!)");
+            Console.WriteLine($"!false:{!false}");
+            Console.WriteLine($"!true:{!true}");
+            Console.ReadLine();
+        }
+    }
+}
